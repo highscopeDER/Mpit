f9bd7014e55b82d3ad27379ab3d958842e12d47d
highscope <highscope@mail.ru>
2022-04-07 16:49:42 +0900

    commit 3

--- app/src/main/java/com/example/mpit/view/loginActivity/LoginPageInterface.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mpit.view.loginActivity
-
-import moxy.MvpView
-import moxy.viewstate.strategy.alias.AddToEnd
-
-@AddToEnd
-interface LoginPageInterface : MvpView {
-
-}
--- app/src/main/java/com/example/mpit/view/loginActivity/LoginPagePresenter.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mpit.view.loginActivity
-
-import moxy.InjectViewState
-import moxy.MvpPresenter
-
-@InjectViewState
-class LoginPagePresenter : MvpPresenter<LoginPageInterface>() {
-
-}
